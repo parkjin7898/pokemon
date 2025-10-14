94cdc32240c8904c6cb2f831ab96520d997eb0d6
parkjin7898 <parkjin7898@naver.com>
2025-10-14 11:46:27 +0900

    🌐한국어

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,13 +2,21 @@ import { Link } from 'react-router-dom'
 
 export default function PokemonCard({ pokemon }) {
   return (
-    <Link to={`/pokemon/${pokemon.id}`} >
+    <Link
+      to={`/pokemon/${pokemon.id}`}
+      className="relative cursor-pointer bg-white shadow-md rounded-2xl p-4 flex flex-col items-center hover:scale-105 transition-transform"
+    >
+      <div className="absolute top-2 left-2 bg-gray-200 text-gray-700 text-xs font-semibold px-2 py-1 rounded-md">
+        {pokemon.id}
+      </div>
+
       <img 
-        src={`https://picsum.photos/128/128?random=${pokemon.id}`} 
+        src={pokemon.image} 
         alt={pokemon.name}
+        className="w-32 h-32 mb-2" 
       />
-      <p>{pokemon.name}</p>
-      <p>#{pokemon.id}</p>
+      <p className="text-lg font-bold">{pokemon.name}</p>
+      {/* <p className="text-gray-500">#{pokemon.id}</p> */}
     </Link>
   )
 }
